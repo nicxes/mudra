7cc445bb1a92d3901bbb8a0239f092901aeae2f1
Nicxes <nicxesh@gmail.com>
2022-07-26 11:35:36 -0300

    Added csv generate file

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,13 +3,44 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Exports\ContactsExport;
-use Maatwebsite\Excel\Facades\Excel;
+use App\Models\Contact;
 
 class ContactController extends Controller
 {
     public function export()
     {
-        return Excel::download(new ContactsExport, 'contacts.xlsx');
+
+
+        $fileName = 'contactos.csv';
+        $contacts = Contact::all();
+
+        $headers = array(
+            "Content-type"        => "text/csv",
+            "Content-Disposition" => "attachment; filename=$fileName",
+            "Pragma"              => "no-cache",
+            "Cache-Control"       => "must-revalidate, post-check=0, pre-check=0",
+            "Expires"             => "0"
+        );
+
+        $columns = array('ID', 'NOMBRE', 'EMAIL', 'TEL', 'DNI', 'FECHA DE REGISTRO');
+        $callback = function() use($contacts, $columns) {
+            $file = fopen('php://output', 'w');
+            fputcsv($file, $columns);
+
+            foreach ($contacts as $contact) {
+                $row['ID']  = $contact->id;
+                $row['NOMBRE']    = $contact->fullname;
+                $row['EMAIL']    = $contact->email;
+                $row['TEL']  = $contact->tel;
+                $row['DNI']  = $contact->dni;
+                $row['FECHA DE REGISTRO']  = $contact->created_at;
+
+                fputcsv($file, array($row['ID'], $row['NOMBRE'], $row['EMAIL'], $row['TEL'], $row['DNI'], $row['FECHA DE REGISTRO']));
+            }
+
+            fclose($file);
+        };
+
+        return response()->stream($callback, 200, $headers);
     }
 }
